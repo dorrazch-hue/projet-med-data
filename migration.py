9d69d1de58ef2917685a530ac2deaa5193100877
DORRA <dorra@MacBook-Pro-de-DORRA.local>
2026-04-20 21:41:20 +0200

    Sécurisation de l'accès BDD et variables d'environnement

--- migration.py
+++ migration.py
@@ -1,18 +1,23 @@
 import pandas as pd
+import os
 from pymongo import MongoClient
 
 try:
-    print("--- Tentative de connexion (Reset) ---")
-    # On utilise 'mongodb' qui est le nom du service dans ton YAML
-    client = MongoClient('mongodb://admin:admin@mongodb:27017/', serverSelectionTimeoutMS=5000)
+    print("--- Tentative de connexion sécurisée ---")
     
-    db = client['healthcare']
+    # Récupération de l'URI depuis les variables d'environnement (configurées dans docker-compose)
+    uri = os.getenv("MONGODB_URI", "mongodb://admin:admin@mongodb:27017/")
+    client = MongoClient(uri, serverSelectionTimeoutMS=5000)
+    
+    # Connexion à la base de données
+    db = client.get_database()
     collection = db['patients']
     
     print("Lecture du fichier CSV...")
     df = pd.read_csv('healthcare_dataset.csv')
     
-    print(f"Envoi de {len(df)} lignes...")
+    print(f"Début de la migration de {len(df)} lignes...")
+    # Insertion par lots pour la performance
     collection.insert_many(df.to_dict(orient='records'))
     
     print("--- MIGRATION RÉUSSIE AVEC SUCCÈS ! ---")
